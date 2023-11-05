f966c982280613f8bae393c7b3e5149b3e835af6
Martin Johansen <martin.nordby.johansen@autodesk.com>
2023-11-04 21:14:26 -0400

    try ooooone more time

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
+const base = process.env.DEV_SERVER == 1 ? "aec-tech-hackathon" : "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    {
+      name: "html-transform",
+      transformIndexHtml: {
+        transform: (html) =>
+          // Ensure resources starting with #REL# are included in the bundle by vite
+          html.replace(/#REL#/g, base),
+      },
+    },
+    preact(),
+  ],
   server: {
     port: 8080,
   },
